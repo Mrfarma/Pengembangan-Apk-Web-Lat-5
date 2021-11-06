6de8dc8dcdab0c6f38965cd5957769392288df7d
Mrfarma <192111237@students.mikroskil.ac.id>
2021-11-06 14:11:17 +0700

    commit on "Tugas 4.2 Simplified"

--- marcolukita/src/components/Header/Header.js
+++ marcolukita/src/components/Header/Header.js
@@ -3,10 +3,10 @@ import "../../css/bootstrap.min.css";
 function Title() {
   return <h2 className='text-center text-info'>Sistem Informasi E-biz</h2>;
 }
-class Header extends React.Component {
-  render() {
-    return <div>{Title()}</div>;
-  }
-}
+// class Header extends React.Component {
+//   render() {
+//     return <div>{Title()}</div>;
+//   }
+// }
 
-export default Header;
+export default Title;
